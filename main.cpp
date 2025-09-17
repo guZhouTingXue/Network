6f8db6d833d03eaae87d5c2034c7fac3eae228a3
guZhouTingXue <2422173022@qq.com>
2025-09-17 21:32:55 +0800

    2.1 套接字协议及其数据传输特性
    TCP 无数据边界特性：一次发送，多次read

--- main.cpp
+++ main.cpp
@@ -7,13 +7,15 @@
 #include <errno.h>
 
 void error_handling(char *message);
+void unix_error(char *msg); /* Unix-style error */
 
 int main(int argc, char* argv[])
 {
     int sock;
     struct sockaddr_in serv_addr;
     char message[30];
-	int str_len;
+	int str_len = 0;
+	int idx = 0, read_len = 0;
 
     if(argc != 3)
     {
@@ -22,7 +24,7 @@ int main(int argc, char* argv[])
     }
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if(sock == -1)
-    	error_handling("serv_socket() error");
+    	unix_error("serv_socket() error");
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
@@ -31,15 +33,20 @@ int main(int argc, char* argv[])
 	
 	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
 	{
-		error_handling("connect() error!");
+		unix_error("connect() error!");
 	}
 	
-	str_len = read(sock, message, sizeof(message) - 1);
-	if(str_len == -1)
-		error_handling("read() error!");
+	while(read_len = read(sock, &message[idx++], 1))
+	{
+		//printf("read_len:%d\n", read_len);
+		if(read_len == -1)
+			unix_error("read() error!");
+		str_len += read_len;
+	}
+	printf("read_len:%d\n", read_len);
 	
 	printf("Message from server :%s\n", message);
-
+	printf("Function read call count: %d\n", str_len);
     close(sock	);
     return 0;
 }
@@ -50,4 +57,10 @@ void error_handling(char *message)
 	fprintf(stderr, " %s", strerror(errno));
 	fputc('\n', stderr);
 	exit(1);
+}
+
+void unix_error(char *msg) /* Unix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
 }
