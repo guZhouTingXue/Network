63e69f18e27478f8bba6d1d58dfde46c01695909
guZhouTingXue <2422173022@qq.com>
2025-10-06 14:54:45 +0800

    6.2 基于UDP的服务端

--- main.cpp
+++ main.cpp
@@ -15,84 +15,37 @@ int main(int argc, char* argv[])
 {
     int sock;
     struct sockaddr_in serv_addr;
-    char message[10] = {0};
-	int str_len = 0;
+    char message[BUF_SIZE];
+    int str_len = 0;
 
     if(argc != 3)
     {
-    	printf("Usage : %s <IP> <port>\n", argv[0]);
-    	exit(1);
+        printf("Usage : %s <IP> <port>\n", argv[0]);
+        exit(1);
     }
-    sock = socket(PF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
     if(sock == -1)
-    	unix_error("serv_socket() error");
+        unix_error("serv_socket() error");
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]); 
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
-    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-        unix_error("connect() error");
-    else
-        puts("Connected ... \n");
 
-    float operand0, operand1;
-    char *p = nullptr;
-    message[0] = 0x66;
+    if(bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        unix_error("bind() error ");
 
-    printf("Operand0:");
-    scanf("%f", &operand0);
-    printf("\n");
-    p = (char*)&operand0;
-    message[1] = *(p + 3);
-    message[2] = *(p + 2);
-    message[3] = *(p + 1);
-    message[4] = *(p + 0);
-
-    printf("Operand1:");
-    scanf("%f", &operand1);
-    printf("\n");
-    p = (char*)&operand0;
-    message[5] = *(p + 3);
-    message[6] = *(p + 2);
-    message[7] = *(p + 1);
-    message[8] = *(p + 0);
-
-    printf("Operator:");
-    scanf(" %c", &message[9]);
-    printf("\n");
-
-    write(sock, message, 10);
-    char recvMsg[BUF_SIZE];
-    int recv_len = 0, recv_cnt = 0;
-    float result = 0.0;
-    bool recvResult = false;
-    while(recv_len < 5)
+    struct sockaddr_in client_addr;
+    socklen_t client_addrlen;
+    while(1)
     {
-        recv_cnt = read(sock, &recvMsg[recv_len], sizeof(recvMsg) - recv_len);
-        recv_len += recv_cnt;
-        if(recv_len >= 5)
-        {
-            for(int i=0; i<recv_len; i++)
-            {
-                if(recvMsg[i] == 0x66)
-                {
-                    char *pr = (char*)&result;
-                    pr[3] = recvMsg[i + 1];
-                    pr[2] = recvMsg[i + 2];
-                    pr[1] = recvMsg[i + 3];
-                    pr[0] = recvMsg[i + 4];
-                    printf("result:%f\n", result);
-                    recvResult = true;
-                }
-            }
-        }
+        str_len = recvfrom(sock, message, BUF_SIZE, 0,
+                           (struct sockaddr*)&client_addr, &client_addrlen);
+        sendto(sock, message, str_len, 0, (struct sockaddr*)&client_addr, client_addrlen);
     }
-    if(!recvResult)
-        printf("Not found\n");
     close(sock);
-	
+
     return 0;
 }
 
