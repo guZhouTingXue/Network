ec855218ac3699ff03754c6dadabd8ea5040b330
guZhouTingXue <2422173022@qq.com>
2025-10-07 20:43:43 +0800

    7.1 基于TCP的半关闭 客户端

--- main.cpp
+++ main.cpp
@@ -24,14 +24,6 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    FILE *fp = NULL;
-    fp = fopen("1.txt", "rb");
-    if(!fp)
-    {
-        perror("fopen() error\n");
-        exit(-1);
-    }
-
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if(sock == -1)
         unix_error("serv_socket() error");
@@ -42,41 +34,28 @@ int main(int argc, char* argv[])
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
-    if(bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-        unix_error("bind() error");
-
-    if(listen(sock, 1) == -1)
-        unix_error("listen() error");
-
-    struct sockaddr_in client_addr;
-    socklen_t client_addrlen;
-    int client_fd;
-    client_fd = accept(sock, (struct sockaddr*)&client_addr, &client_addrlen);
-    if( client_fd == -1)
-        unix_error("listen() error");
     int read_cnt = 0;
-
-    while(1)
+    FILE *fp = fopen("receive.txt", "wb");
+    if(!fp)
     {
-        read_cnt = fread((void*)message, 1, 2, fp);
-        if(read_cnt < 2)
-        {
-            write(client_fd, message, read_cnt);
-            break;
-        }
-        write(client_fd, message, 2);
+        printf("fopen() error");
+        exit(-1);
     }
 
-    shutdown(client_fd, SHUT_WR);
-    printf("shutdown\n");
-    read_cnt = read(client_fd, message, BUF_SIZE -1);
-    message[read_cnt] = 0;
-    printf("Message from client: %s\n", message);
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        unix_error("connect() error");
+    printf("connected\n");
+    while( (read_cnt = read(sock, message, 1)) != 0 )
+    {
+        //message[read_cnt] = 0;
+        //printf("data:%s\n", message);
+        fwrite((void*)message, 1, read_cnt, fp);
+    }
 
+    puts("Received file data");
+    write(sock, "Thank you", 10);
     fclose(fp);
-    close(client_fd);
     close(sock);
-
     return 0;
 }
 
