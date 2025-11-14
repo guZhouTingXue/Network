28c14bdf0c9d1fe0294d452250348b6df248a0f6
guZhouTingXue <2422173022@qq.com>
2025-11-14 17:36:27 +0800

    8.2 IP地址和域名之间的转换 gethostbyaddr

--- main.cpp
+++ main.cpp
@@ -14,13 +14,16 @@ void unix_error(char *msg); /* Unix-style error */
 
 int main(int argc, char* argv[])
 {
-    const char *hostname = "baidu.com";
-    struct hostent *host = gethostbyname(hostname);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    struct hostent *host = gethostbyaddr((void*)&addr.sin_addr, sizeof(addr.sin_addr), AF_INET);
     if(!host)
     {
-        fprintf(stderr, "gethost error: %s", hstrerror(h_errno));
-        exit(1);
+         fprintf(stderr, "gethost error: %s\n", hstrerror(h_errno));
+         exit(1);
     }
+    //打印host 信息 和 前面一致
     printf("name:%s\n", host->h_name);
     for(int i=0; host->h_aliases[i]; i++)
         printf("Aliases %d: %s \n", host->h_aliases[i]);
