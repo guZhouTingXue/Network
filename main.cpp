75eb668a37788d6a69d082eb59eadd6612cabf66
guZhouTingXue <2422173022@qq.com>
2025-11-14 16:38:33 +0800

    8.2 IP地址和域名之间的转换 gethostbyname

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <errno.h>
+#include <netdb.h>
 
 #define BUF_SIZE 1024
 
@@ -13,49 +14,23 @@ void unix_error(char *msg); /* Unix-style error */
 
 int main(int argc, char* argv[])
 {
-    int sock;
-    struct sockaddr_in serv_addr;
-    char message[BUF_SIZE];
-    int str_len = 0;
-
-    if(argc != 3)
+    const char *hostname = "baidu.com";
+    struct hostent *host = gethostbyname(hostname);
+    if(!host)
     {
-        printf("Usage : %s Server<ip> <port> \n", argv[0]);
+        fprintf(stderr, "gethost error: %s", hstrerror(h_errno));
         exit(1);
     }
+    printf("name:%s\n", host->h_name);
+    for(int i=0; host->h_aliases[i]; i++)
+        printf("Aliases %d: %s \n", host->h_aliases[i]);
+    printf("Address type:%s\n",
+           (host->h_addrtype == AF_INET) ? "AF_INET" : "AF_INET6");
+    for(int i=0; host->h_addr_list[i]; i++)
+        printf("IP addr %d: %s \n", i+1,
+               inet_ntoa(*(struct in_addr*)host->h_addr_list[i]));
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock == -1)
-        unix_error("serv_socket() error");
 
-
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port = htons(atoi(argv[2]));
-
-    int read_cnt = 0;
-    FILE *fp = fopen("receive.txt", "wb");
-    if(!fp)
-    {
-        printf("fopen() error");
-        exit(-1);
-    }
-
-    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-        unix_error("connect() error");
-    printf("connected\n");
-    while( (read_cnt = read(sock, message, 1)) != 0 )
-    {
-        //message[read_cnt] = 0;
-        //printf("data:%s\n", message);
-        fwrite((void*)message, 1, read_cnt, fp);
-    }
-
-    puts("Received file data");
-    write(sock, "Thank you", 10);
-    fclose(fp);
-    close(sock);
     return 0;
 }
 
