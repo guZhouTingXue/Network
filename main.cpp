87c9b93e2aeafffd75832a737bd95199b890644f
guZhouTingXue <2422173022@qq.com>
2025-11-29 20:37:20 +0800

    9.2 Time-wait 状态 Address already in use

--- main.cpp
+++ main.cpp
@@ -38,6 +38,45 @@ int main(int argc, char* argv[])
             printf("TCP sock sendbuffer size changed to : %d\n", buf_size);
     }
 
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+        unix_error("serv_socket() error");
+
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if(bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        unix_error("bind() error");
+
+    if(listen(sock, 1) == -1)
+        unix_error("listen() error");
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addrlen;
+    int client_fd;
+    client_fd = accept(sock, (struct sockaddr*)&client_addr, &client_addrlen);
+    if( client_fd == -1)
+        unix_error("listen() error");
+    int read_cnt = 0;
+
+    while(1)
+    {
+         read_cnt = read(client_fd, message, sizeof(message)); 
+         if(read_cnt == 0)
+            break;
+        write(client_fd, message, read_cnt);
+    }
+
+    shutdown(client_fd, SHUT_WR);
+    printf("shutdown\n");
+    read_cnt = read(client_fd, message, BUF_SIZE -1);
+    message[read_cnt] = 0;
+    printf("Message from client: %s\n", message);
+
+    close(client_fd);
     close(sock);
 
     return 0;
